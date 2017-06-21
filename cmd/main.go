1f81e479330d421380a85f582c8d86099abc9395
Dillon Buchanan <dbuchanan@128technology.com>
2017-06-21 11:04:14 -0400

    Removed per-router concurrency for router-wide concurrency.
    - max-concurrent-routers is a new argument that specifies how many
      routers to query concurrently. The more routers that are queried the
      more CPU and load on influx there will be - so there is a sweet spot
      somewhere.

--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/jeffail/tunny"
+	"github.com/abiosoft/semaphore"
 
 	t128 "github.com/128technology/influx-importer/client"
 	influx "github.com/128technology/influx-importer/influx"
@@ -26,13 +26,13 @@ var (
 )
 
 var (
-	token          = kingpin.Flag("token", "The API token.").Short('t').Required().OverrideDefaultFromEnvar("TOKEN").String()
-	url            = kingpin.Flag("url", "The url URL.").Short('u').Required().String()
-	workers        = kingpin.Flag("workers", "The number of works to process data.").Short('w').Default("2").Int()
-	influxAddress  = kingpin.Flag("influx-address", "The HTTP address of the influx instance.").Required().String()
-	influxUser     = kingpin.Flag("influx-user", "The username for the influx instance.").Default("").OverrideDefaultFromEnvar("INFLUX_USER").String()
-	influxPass     = kingpin.Flag("influx-pass", "The password for the influx instance.").Default("").OverrideDefaultFromEnvar("INFLUX_PASS").String()
-	influxDatabase = kingpin.Flag("influx-database", "The influx database to store analytics.").Required().String()
+	token                = kingpin.Flag("token", "The API token.").Short('t').Required().OverrideDefaultFromEnvar("TOKEN").String()
+	url                  = kingpin.Flag("url", "The url URL.").Short('u').Required().String()
+	influxAddress        = kingpin.Flag("influx-address", "The HTTP address of the influx instance.").Required().String()
+	influxUser           = kingpin.Flag("influx-user", "The username for the influx instance.").Default("").OverrideDefaultFromEnvar("INFLUX_USER").String()
+	influxPass           = kingpin.Flag("influx-pass", "The password for the influx instance.").Default("").OverrideDefaultFromEnvar("INFLUX_PASS").String()
+	influxDatabase       = kingpin.Flag("influx-database", "The influx database to store analytics.").Required().String()
+	maxConcurrentRouters = kingpin.Flag("max-concurrent-routers", "The number of routers to query concurrently.").Short('r').Default("10").Int()
 )
 
 func parametersToString(parameters []t128.AnalyticParameter) string {
@@ -61,41 +61,28 @@ func extract() error {
 		return err
 	}
 
-	var wg sync.WaitGroup
-
-	pool, err := tunny.CreatePoolGeneric(*workers).Open()
-	if err != nil {
-		return err
-	}
-
-	enqueueAnalytics := func(metrics []string, parameters []t128.AnalyticParameter) {
+	extractAndSend := func(metrics []string, parameters []t128.AnalyticParameter) {
 		paramStr := parametersToString(parameters)
 
 		for _, metric := range metrics {
-			wg.Add(1)
-
-			pool.SendWork(func() {
-				defer wg.Done()
-
-				points, err := client.GetAnalytic(&t128.AnalyticRequest{
-					Metric:     metric,
-					Transform:  "sum",
-					Window:     window,
-					Parameters: parameters,
-				})
-
-				if err != nil {
-					stderr.Printf("HTTP request for %v(%v) failed return successfully: %v\n", metric, paramStr, err.Error())
-					return
-				}
-
-				if err = influxClient.Send(metric, parameters, points); err != nil {
-					stderr.Printf("Influx write for %v(%v) failed return successfully: %v\n", metric, paramStr, err.Error())
-					return
-				}
-
-				stdout.Printf("Successfully exported %v(%v).", metric, paramStr)
+			points, err := client.GetAnalytic(&t128.AnalyticRequest{
+				Metric:     metric,
+				Transform:  "sum",
+				Window:     window,
+				Parameters: parameters,
 			})
+
+			if err != nil {
+				stderr.Printf("HTTP request for %v(%v) failed return successfully: %v\n", metric, paramStr, err.Error())
+				continue
+			}
+
+			if err = influxClient.Send(metric, parameters, points); err != nil {
+				stderr.Printf("Influx write for %v(%v) failed return successfully: %v\n", metric, paramStr, err.Error())
+				continue
+			}
+
+			stdout.Printf("Successfully exported %v(%v).", metric, paramStr)
 		}
 	}
 
@@ -111,62 +98,73 @@ func extract() error {
 		}
 	}
 
-	for _, router := range config.Authority.Routers {
-		for _, node := range router.Nodes {
-			enqueueAnalytics(t128.NodeMetrics, []t128.AnalyticParameter{
-				{Name: "router", Value: router.Name},
-				{Name: "node", Value: node.Name},
-			})
+	var wg sync.WaitGroup
+	sem := semaphore.New(*maxConcurrentRouters)
 
-			for _, deviceInterface := range node.DeviceInterfaces {
-				enqueueAnalytics(t128.DeviceInterfaceMetrics, []t128.AnalyticParameter{
+	for _, router := range config.Authority.Routers {
+		wg.Add(1)
+
+		go func(router t128.Router) {
+			sem.Acquire()
+			defer sem.Release()
+			defer wg.Done()
+
+			for _, node := range router.Nodes {
+				extractAndSend(t128.NodeMetrics, []t128.AnalyticParameter{
 					{Name: "router", Value: router.Name},
-					{Name: "device_interface", Value: strconv.Itoa(deviceInterface.ID)},
+					{Name: "node", Value: node.Name},
 				})
 
-				for _, networkInterface := range deviceInterface.NetworkInterfaces {
-					enqueueAnalytics(t128.NetworkInterfaceMetrics, []t128.AnalyticParameter{
+				for _, deviceInterface := range node.DeviceInterfaces {
+					extractAndSend(t128.DeviceInterfaceMetrics, []t128.AnalyticParameter{
 						{Name: "router", Value: router.Name},
-						{Name: "network_interface", Value: networkInterface.Name},
+						{Name: "device_interface", Value: strconv.Itoa(deviceInterface.ID)},
 					})
+
+					for _, networkInterface := range deviceInterface.NetworkInterfaces {
+						extractAndSend(t128.NetworkInterfaceMetrics, []t128.AnalyticParameter{
+							{Name: "router", Value: router.Name},
+							{Name: "network_interface", Value: networkInterface.Name},
+						})
+					}
 				}
 			}
-		}
 
-		for _, service := range config.Authority.Services {
-			enqueueAnalytics(t128.ServiceMetrics, []t128.AnalyticParameter{
-				{Name: "router", Value: router.Name},
-				{Name: "service", Value: service.Name},
-			})
-		}
+			for _, service := range config.Authority.Services {
+				extractAndSend(t128.ServiceMetrics, []t128.AnalyticParameter{
+					{Name: "router", Value: router.Name},
+					{Name: "service", Value: service.Name},
+				})
+			}
 
-		for _, tenant := range config.Authority.Tenants {
-			enqueueAnalytics(t128.TenantMetrics, []t128.AnalyticParameter{
-				{Name: "router", Value: router.Name},
-				{Name: "tenant", Value: tenant.Name},
-			})
-		}
+			for _, tenant := range config.Authority.Tenants {
+				extractAndSend(t128.TenantMetrics, []t128.AnalyticParameter{
+					{Name: "router", Value: router.Name},
+					{Name: "tenant", Value: tenant.Name},
+				})
+			}
 
-		for _, serviceClass := range config.Authority.ServiceClasses {
-			enqueueAnalytics(t128.ServiceClassMetrics, []t128.AnalyticParameter{
-				{Name: "router", Value: router.Name},
-				{Name: "service_class", Value: serviceClass.Name},
-			})
-		}
+			for _, serviceClass := range config.Authority.ServiceClasses {
+				extractAndSend(t128.ServiceClassMetrics, []t128.AnalyticParameter{
+					{Name: "router", Value: router.Name},
+					{Name: "service_class", Value: serviceClass.Name},
+				})
+			}
 
-		for _, serviceRoute := range router.ServiceRoutes {
-			enqueueAnalytics(t128.ServiceRouteMetrics, []t128.AnalyticParameter{
-				{Name: "router", Value: router.Name},
-				{Name: "service_route", Value: serviceRoute.Name},
-			})
-		}
+			for _, serviceRoute := range router.ServiceRoutes {
+				extractAndSend(t128.ServiceRouteMetrics, []t128.AnalyticParameter{
+					{Name: "router", Value: router.Name},
+					{Name: "service_route", Value: serviceRoute.Name},
+				})
+			}
 
-		for serviceGroup := range serviceGroups {
-			enqueueAnalytics(t128.ServiceGroupMetrics, []t128.AnalyticParameter{
-				{Name: "router", Value: router.Name},
-				{Name: "service_group", Value: serviceGroup},
-			})
-		}
+			for serviceGroup := range serviceGroups {
+				extractAndSend(t128.ServiceGroupMetrics, []t128.AnalyticParameter{
+					{Name: "router", Value: router.Name},
+					{Name: "service_group", Value: serviceGroup},
+				})
+			}
+		}(router)
 	}
 
 	wg.Wait()
@@ -176,6 +174,11 @@ func extract() error {
 func main() {
 	kingpin.Version(build)
 	kingpin.Parse()
+
+	if *maxConcurrentRouters <= 0 {
+		stderr.Println("Error: The maximum concurrent routers must be greater than 0")
+		os.Exit(-1)
+	}
 
 	if err := extract(); err != nil {
 		panic(err)
